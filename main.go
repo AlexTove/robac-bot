f5a7c11e7702665a48f0aead3c701b54532f75b9
SCWebenizer <horia_vlad.onofrei@stud.acs.upb.ro>
2020-07-15 09:23:21 +0300

    Jumped to package-based structure

--- main.go
+++ main.go
@@ -4,12 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
-	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"os/signal"
-	"regexp"
 	"strings"
 	"syscall"
 )
@@ -88,76 +85,5 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		_, _ = fmt.Fscanf(r, "$%s", &cmd)
 
 		processCommand(s, m, r, &cmd)
-	}
-}
-
-func processCommand(s *discordgo.Session, m *discordgo.MessageCreate, r *strings.Reader, cmd *string) {
-	switch *cmd {
-	case "Baciu":
-		_, _ = s.ChannelMessageSend(m.ChannelID, "E cam gay")
-
-	case "kick":
-		userPermission, _ := s.UserChannelPermissions(m.Author.ID, m.ChannelID)
-
-		if userPermission & discordgo.PermissionKickMembers == 0 ||
-			userPermission & discordgo.PermissionAdministrator == 0 {
-			return
-		}
-
-		var userid, reason string
-
-		_, _ = fmt.Fscanf(r, " %s", &userid)
-
-		reg, err := regexp.Compile("[^0-9]+")
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		userid = reg.ReplaceAllString(userid, "")
-
-		if len(userid) == 0 {
-			return
-		}
-
-		buf := new(strings.Builder)
-		_, _ = io.Copy(buf, r)
-
-		if len(buf.String()) == 0 {
-			reason = "No reason was given."
-		} else {
-			reason = buf.String()
-		}
-
-		err = s.GuildMemberDelete(m.GuildID, userid)
-
-		if userid == m.Author.ID {
-			_, _ = s.ChannelMessageSend(m.ChannelID, "(" + m.Author.Username + ")" +
-				" You can't kick yourself!")
-			return
-		}
-
-		if err != nil {
-			_, _ = s.ChannelMessageSend(m.ChannelID, "(" + m.Author.Username + ")" +
-				" You can't kick that user!")
-			return
-		} else {
-			_, _ = s.ChannelMessageSend(m.ChannelID, "(" + m.Author.Username + ")" +
-				" User kicked successfully!")
-		}
-
-		privateChannel, err := s.UserChannelCreate(userid)
-
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		_, err = s.ChannelMessageSend(privateChannel.ID, "You have been kicked from the server.\n" +
-			"Reason: " + reason + "\n" +
-			"Author: " + m.Author.Username)
-
-		if err != nil {
-			log.Fatal(err)
-		}
 	}
 }
